d229df50f4d094a62deb8c5e28f359cc124ad3d2
Nirav Joshi <joshi186@avaya.com>
2020-07-22 13:55:59 +0530

    Adding changes related to oracle database connectivity

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,12 +1,12 @@
 import {TypeOrmModuleOptions} from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-    type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'Joshinirav@21',
-    database: 'taskmanagement',
+    type: 'oracle',
+    connectString:'dlesfvmdb01.nonprod.avaya.com:1526/esfdev',
+    host: 'dlesfvmdb01.nonprod.avaya.com',
+    port: 1526,
+    username: 'aes',
+    password: 'Wol#1ErsIpY#UF$dLzfk6miDSds_27',
     entities: [__dirname + '/../**/*.entity{.ts,.js}'],
     synchronize: true,
 };
